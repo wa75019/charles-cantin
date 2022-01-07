b47f0d2d6170bbb5c7a961940668d9d360edd537
Benoit Laurore <benoit.laurore@bbox.fr>
2022-01-07 18:16:03 +0100

    changes on gallery page

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -4,29 +4,34 @@ import Layout from '../../components/layout/layout'
 import { MdKeyboardArrowDown } from 'react-icons/md'
 import { getGalleryCategories, getCategoryContent } from '../../components/posts'
 import Link from 'next/link'
+import Masonry from 'react-masonry-css'
 
 
 
 export default function Gallery({ allCategories, allPhotoData }) {
 
 const [fileName, setFileName] = useState('')
- const handleClick = ( e, name ) => {
+
+const handleClick = ( e, name ) => {
   e.preventDefault
   setFileName(name)
 }
 const styledFileName = fileName.toLowerCase().replace(/[éèê]/g, "e")
 var imgSrc = null
-let content
+
 allPhotoData.forEach(element => {
   if (element.id === styledFileName) {
     imgSrc = element.images
-    
   }
   else {
     imgSrc = []
   }
 })
 console.log(imgSrc)
+imgSrc = imgSrc.map(function(item){
+  return <img src={item.thumbnail} key={item.thumbnail} /> 
+})
+
   return (
     <Layout>
       <p className={styles.textCategory}>Cliquez sur une catégorie</p>
@@ -34,18 +39,25 @@ console.log(imgSrc)
       <div className={styles.categories}>
         {allCategories.map(({ name, thumbnail }) => (
           <Link href="#" key={name} >
-            <a className={styles.category} onClick={(e) => handleClick(e, name)}>
+            <a className={`${styles.category} ${fileName == name ? "active" : ""}`} onClick={(e) => handleClick(e, name)} >
               <img src={thumbnail} alt={name} />
               <p>{name}</p>
             </a>
           </Link>
             ))}
       </div>
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
+        {imgSrc}
+      </Masonry>
 
-      {imgSrc.map(({ thumbnail }) => (
+        {/* 
+      {imgSrc = imgSrc.map(({ thumbnail }) => (
         <img src={thumbnail} key={thumbnail} /> 
-      
-      ))}
+        ))}
+      */}
 
           
     </Layout>
